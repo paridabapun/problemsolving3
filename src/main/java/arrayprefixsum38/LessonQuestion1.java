3ff56e2a833f7c5b57918f267987abda82859432
Bapun Parida <pariabapun@gmail.com>
2023-03-24 00:17:45 +0530

    Added

--- src/main/java/arrayprefixsum38/LessonQuestion1.java
+++ src/main/java/arrayprefixsum38/LessonQuestion1.java
@@ -4,8 +4,26 @@ package arrayprefixsum38;
  * Given n array elements , q queries , for the ith queries , calculate sum of all the elements in the range [li, ri]
  */
 public class LessonQuestion1 {
-	
-	
-	
+
+	static void rangeQueries(int arr[], int n, int q) {
+
+		int pref[] = new int[n];
+		pref[0] = arr[0];
+
+		for (int i = 1; i < n; i++) {
+			pref[i] = pref[i - 1] + arr[i];
+		}
+
+		for (int i = 0; i < q; i++) {
+			int l = 3;
+			int r = 7;
+			if (l == r) {
+				System.out.println(pref[r]);
+			} else {
+				System.out.println(pref[r] - pref[l - 1]);
+			}
+		}
+
+	}
 
 }
