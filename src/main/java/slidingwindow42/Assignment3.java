66c9b309b298a6fe53dff50a7c51d17088362fd5
Bapun Parida <paridabapun@gmail.com>
2023-05-19 18:57:37 +0530

    Added

--- src/main/java/slidingwindow42/Assignment3.java
+++ src/main/java/slidingwindow42/Assignment3.java
@@ -1,53 +1,115 @@
 package slidingwindow42;
 
+
+/*
+ * 
+ * Q3. Spiral Order Matrix II
+
+ * 
+ * Problem Description
+Given an integer A, generate a square matrix filled with elements from 1 to A2 in spiral order and return the generated square matrix.
+
+
+
+Problem Constraints
+1 <= A <= 1000
+
+
+
+Input Format
+First and only argument is integer A
+
+
+Output Format
+Return a 2-D matrix which consists of the elements added in spiral order.
+
+
+
+Example Input
+Input 1:
+
+1
+Input 2:
+
+2
+Input 3:
+
+5
+
+
+Example Output
+Output 1:
+
+[ [1] ]
+Output 2:
+
+[ [1, 2], 
+  [4, 3] ]
+Output 2:
+
+[ [1,   2,  3,  4, 5], 
+  [16, 17, 18, 19, 6], 
+  [15, 24, 25, 20, 7], 
+  [14, 23, 22, 21, 8], 
+  [13, 12, 11, 10, 9] ]
+
+
+Example Explanation
+Explanation 1:
+
+Only 1 is to be arranged.
+Explanation 2:
+
+1 --> 2
+      |
+      |
+4<--- 3
+Explanation 3:
+
+
+
+ */
 public class Assignment3 {
-	
 
-	public static int[][] generateMatrix(int A[][]) {
-		
-		
-		
+	public int[][] generateMatrix(int A) {
 
-		
-		int n =0;
-		 int mat[][] = new int[n][n];
+		int mat[][] = new int[A][A];
 		int i = 0;
 		int j = 0;
-		while (n > 1) {
+		int value = 1;
+		while (A > 1) {
 
-			for (int k = 1; k <= n - 1; k++) {
-				mat[i][j] = A[i][j];
+			for (int k = 1; k < A; k++) {
+				mat[i][j] = value;
+				value++;
 
 				j++;
 			}
 
-			for (int k = 1; k <= n - 1; k++) {
-				mat[i][j] = A[i][j];
+			for (int k = 1; k < A; k++) {
+				mat[i][j] = value;
+				value++;
 				i++;
 			}
-			for (int k = 1; k <= n - 1; k++) {
-				mat[i][j] = A[i][j];
+			for (int k = 1; k < A; k++) {
+				mat[i][j] = value;
+				value++;
 				j--;
 			}
-			for (int k = 1; k <= n - 1; k++) {
-				mat[i][j] = A[i][j];
+			for (int k = 1; k < A; k++) {
+				mat[i][j] = value;
+				value++;
 				i--;
 			}
 			i++;
 			j++;
-			n -= 2;
+			A -= 2;
 		}
-
-	
-
+		if (A == 1) {
+			mat[i][j] = value;
+		}
 		return mat;
-	}
-	public static void main(String[] args) {
 
-		int mat1[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-
-		generateMatrix(mat1);
-		
 	}
 
 }
